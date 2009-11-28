8149277dfd42062ac4b73c53ac06d8a1a245c15f
Alexander Surma <surma@drebesium.org>
2009-11-28 22:29:38 +0100

    isdir() fix

--- tombstone.c
+++ tombstone.c
@@ -10,10 +10,10 @@
 
 #define _calloc(n,t) (t*)malloc(n*sizeof(t)) ;
 
-int _isdir(char *path) ;
-int isdir(char *path, char *file) ;
-void dirname(char *dst, char *src, int n) ;
 void die(char *s) ;
+void dirname(char *dst, char *src, int n) ;
+int isdir(char *path, char *file) ;
+int isfiltered(char *filename) ;
 
 void die(char *s) {
 	fputs(s, stdout) ;
@@ -32,21 +32,22 @@ void dirname(char *dst, char *src, int n) {
 	}
 }
 
-int _isdir(char *path) {
+int isdir(char *path, char *file) {
+	int ret ;
+	char *c ;
 	struct stat f ;
 
-	if(!stat(path, &f))
-		return 0 ;
-	return S_ISDIR(f.st_mode) ;
-}
-
-int isdir(char *path, char *file) {
-	char c[MAXPATH] ;
-
+	c = _calloc(strlen(path)+strlen(file)+2, char) ;
+	if(!c)
+		die("Couldn't allocate memory") ;
 	strcpy (c, path);
 	strcat (c, "/") ;
 	strcat (c, file) ;
-	return _isdir(c) ;
+	ret = stat(c, &f) ;
+	free(c) ;
+	if(ret != 0) 
+		return 0 ;
+	return f.st_mode  & 040000;
 }
 
 int main(int argc, char **argv, char **env) {
@@ -57,15 +58,14 @@ int main(int argc, char **argv, char **env) {
 		die("tombstone-"VERSION" (c) 2009 Alexander Surma\n") ;
 
 	printf("<html><body bgcolor=\"#000000\" text=\"#00FF00\"><pre>\n") ;
+	curdir = _calloc(200, char) ;
 	dirname(curdir, argv[1], 200) ;
-	printf("%s\n", curdir) ;
 	d = opendir(curdir) ;	
 	if(!d) 
 		die("Couldn't open directory\n") ;
 
 	while((c = readdir(d)) != NULL) {
-		//printf("%d, %s\n", isdir(curdir, c->d_name), c->d_name) ;
-		printf("%s\n",  c->d_name) ;
+		printf("%s - %s\n", isdir(curdir, c->d_name)?"d":"f", c->d_name) ;
 	}
 	closedir(d) ;
 	printf("</pre></body></html>\n") ;
