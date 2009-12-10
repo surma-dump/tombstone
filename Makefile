66a32fc2b1326d772dfe781e329e136d53aa3275
Alexander Surma <surma@drebesium.org>
2009-12-11 00:19:44 +0100

    Beautification, structure, first signs of intelligence
    - Alphabetically ordered prototypes
    - First attempt on tree structure

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-CFLAGS=-I. -I/usr/include 
+CFLAGS=-I. -I/usr/include `pkg-config --cflags glib-2.0`
+LDFLAGS=`pkg-config --libs glib-2.0`
 all:
-	gcc tombstone.c -o tombstone ${CFLAGS}
-	gcc tombstone.c -o tombstone_debug -ggdb ${CFLAGS}
+	gcc tombstone.c -o tombstone ${CFLAGS} ${LDFLAGS}
+	gcc tombstone.c -o tombstone_debug -ggdb ${CFLAGS} ${LDFLAGS}
 gdb:
 	gdb ./tombstone_debug
 run:
--- tombstone.c
+++ tombstone.c
@@ -1,73 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <dirent.h>
+
+#define _calloc(n,t) (t*)malloc(n*sizeof(t)) 
+#define F_DIR		00
+#define F_FILE		01
+#define F_SELECTED	02
+
+char *dirpath ;
+
+struct dir {
+	char *name ;
+	short int flags ;
+	struct *dir s ;
+	struct *dir c ;
+} 
+
+
+struct dir *build_tree (const char *path) ;
+void die(char *s) ;
+void prepare_path (char *t, const char *s1, const char *s2) ;
+
 
 #include "config.h"
 
-#define _calloc(n,t) (t*)malloc(n*sizeof(t)) ;
 
-void die(char *s) ;
-void dirname(char *dst, char *src, int n) ;
-int isdir(char *path, char *file) ;
-int isfiltered(char *filename) ;
+struct dir *build_tree (const char *path) {
+	struct dir *ret ;
+	struct dirent *e ;
+	struct stat *s ;
+	DIR *d = opendir(path) ;
+	if (!d)
+		return NULL ;
+	while((e = readdir(path)) != NULL) {
+	}
+
+}
 
 void die(char *s) {
 	fputs(s, stdout) ;
 	exit(EXIT_FAILURE) ;
 }
 
-void dirname(char *dst, char *src, int n) {
-	int i, max = strlen(src) ;
-	n -= 2 ;
-	if (max > n) max = n ;
-	for(i = max; i >= 0; i--) {
-		if(src[i] == '/') {
-			strncpy(dst, src, i) ;
-			return ;
-		}
-	}
+void prepare_path (char *t, const char *s1, const char *s2) {
+	strcpy(t,s1) ;
+	strcat(t,s2) ;
 }
 
-int isdir(char *path, char *file) {
-	int ret ;
-	char *c ;
-	struct stat f ;
 
-	c = _calloc(strlen(path)+strlen(file)+2, char) ;
-	if(!c)
-		die("Couldn't allocate memory") ;
-	strcpy (c, path);
-	strcat (c, "/") ;
-	strcat (c, file) ;
-	ret = stat(c, &f) ;
-	free(c) ;
-	if(ret != 0) 
-		return 0 ;
-	return f.st_mode  & 040000;
-}
 
 int main(int argc, char **argv, char **env) {
-	char *curdir ;
-	DIR *d ;
-	struct dirent *c ;
 	if (argc != 2)
 		die("tombstone-"VERSION" (c) 2009 Alexander Surma\n") ;
-
-	printf("<html><body bgcolor=\"#000000\" text=\"#00FF00\"><pre>\n") ;
-	curdir = _calloc(200, char) ;
-	dirname(curdir, argv[1], 200) ;
-	d = opendir(curdir) ;	
-	if(!d) 
-		die("Couldn't open directory\n") ;
-
-	while((c = readdir(d)) != NULL) {
-		printf("%s - %s\n", isdir(curdir, c->d_name)?"d":"f", c->d_name) ;
-	}
-	closedir(d) ;
-	printf("</pre></body></html>\n") ;
-	free(curdir) ;
+	dirpath = _calloc(strlen(argv[1]+strlen(ROOT)),char) ;
+	prepare_path(dirpath, ROOT, argv[1]) ;
+	head = build_tree(ROOT) ;
+	free_tree(head)
+	free(dirpath) ;
 }
