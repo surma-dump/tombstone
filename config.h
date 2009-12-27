028d27e3fb07ceb03666a43ab55ace25c2477c86
Alexander Surma <surma@drebesium.org>
2009-12-27 19:24:44 +0100

    Copy'n'Paste mistake

--- config.h
+++ config.h
@@ -3,4 +3,4 @@
 hash_func hash = &modulo_hash ;
 char *ROOT = "/home/surma/lighttpd/docroot" ;
 char *HEADER = "/home/surma/lighttpd/docroot/header.html" ;
-char *FOOTER = "/home/surma/lighttpd/docroot/header.html" ;
+char *FOOTER = "/home/surma/lighttpd/docroot/footer.html" ;
